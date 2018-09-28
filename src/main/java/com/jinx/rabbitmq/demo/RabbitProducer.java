75bb87d015b2ac62032250285dc4554a8fe21f1f
liufeng <631819061@qq.com>
2018-09-28 10:47:11 +0800

    Initial commit

---
+++ src/main/java/com/jinx/rabbitmq/demo/RabbitProducer.java
@@ -0,0 +1,10 @@
+package com.jinx.rabbitmq.demo;
+
+/**
+ * @author jinx
+ * @date 2018/9/28 10:43
+ * Desc:
+ */
+public class RabbitProducer {
+    private static final String EXCHANTE_NAME = "exchange_demo";
+}
